b12f5ccf32134633e82f5570af2fe4dc3fbe1480
Todd A Nordquist <toddnordquist144@gmail.com>
2019-02-19 18:01:32 -0700

    Added recursive method called reducedDigitSum and tested.

--- src/edu/cnm/deepdive/Check.java
+++ src/edu/cnm/deepdive/Check.java
@@ -8,33 +8,50 @@ public class Check {
 
     int dividend = input;
     final int DIVISOR = 10;
-    int quotient = dividend/DIVISOR;
-    int remainder = dividend%DIVISOR;
-    int sumRemainders=0;
-    sumRemainders+= remainder;
+    int quotient = dividend / DIVISOR;
+    int remainder = dividend % DIVISOR;
+    int sumRemainders = 0;
+    sumRemainders += remainder;
 
-    while(quotient>0) {
+    while (quotient > 0) {
       dividend = quotient;
-      quotient = dividend/DIVISOR;
-      remainder=dividend%DIVISOR;
-      sumRemainders+=remainder;
+      quotient = dividend / DIVISOR;
+      remainder = dividend % DIVISOR;
+      sumRemainders += remainder;
     }
 
     return sumRemainders;
   }
 
+  /*
+  Write the implementation of the method, so that it behaves like digitSum,
+  except that after it adds the digits together, it continues doing so, until we
+  have a single digit number. For example, if input has the value 147, then the
+  digit sum is 1 + 4 + 7 = 12. However, for this method, we would repeat that
+  process until we have a single digit number. So after getting a sum of 12, we
+  add those digits together to get 1 + 2 = 3. Thus, in this invocation, this
+  method should return 3.
+   */
+  public static int reducedDigitSum(int input) {
+
+    if (input < 10) {
+      return input;
+    } else {
+
+      return reducedDigitSum(digitSum(input));
+
+    }
+  }
+
   public static void main(String[] args) {
 
-    int fromCom = parseInt(args[0]);
+    //int fromCom = parseInt(args[0]);
+    int fromCom = 4046;
 
-    int sumDigits = digitSum(fromCom);
-    final int DIVISOR = 9;
-    System.out.printf("\n");
-    System.out.printf("%5d\n", fromCom);
-    System.out.printf("%5d\n", fromCom%DIVISOR);
-    System.out.printf("%5d\n", sumDigits);
-    System.out.printf("%5d\n", sumDigits%DIVISOR);
-    System.out.println();
+    int singleDigit = reducedDigitSum(fromCom);
+
+    System.out.printf("%4d", singleDigit);
+
 
   }
 
--- test/edu/cnm/deepdive/CheckTest.java
+++ test/edu/cnm/deepdive/CheckTest.java
@@ -8,6 +8,7 @@ class CheckTest {
 
   static final int[] INPUT = {4046,6325,9788,5676,88,8925,8598,2025,4738,453};
   static final int[] EXPECTED = {14,16,32,24,16,24,30,9,22,12};
+  static final int[] REDUCED_EXPECTED = {5,7,5,6,7,6,3,9,4,3};
 
   @Test
   void digitSum() {
@@ -19,5 +20,16 @@ class CheckTest {
       assertEquals(expected,actual);
     }
 
+  }
+
+  @Test
+  void reducedDigitSum() {
+
+    for(int i = 0; i<INPUT.length; ++i) {
+      int input = INPUT[i];
+      int expected = REDUCED_EXPECTED[i];
+      int actual = Check.reducedDigitSum(input);
+      assertEquals(expected,actual);
+    }
   }
 }
